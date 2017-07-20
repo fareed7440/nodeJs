9718fbf5eb2d41446b92400ea5252dfea04badc5
fareed7440 <fareed7440@gmail.com>
2017-07-20 08:03:43 -0700

    more updated

--- app.js
+++ app.js
@@ -1,13 +1,129 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongoose  =  require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017")
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/Fareed021")
 var app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+var flash = require('connect-flash');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 var Bear = require('./bear.js')
-var port = process.env.PORT || 8081 ; 
-var router  = express.Router();
+var port = process.env.PORT || 8081;
+var router = express.Router();
+ 
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.use(allowCrossDomain);
+router.use(function (req, res, next) {
+    // do logging
+
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+router.route('/bears')
+    .post(function (req, res) {
+
+        var bear = new Bear();
+
+        console.log("This request body", req.body);
+        bear.name = req.body.data.data;
+        console.log('response', req.body.data.data)
+
+        console.log("here is ", bear)
+        bear.save(function (err) { 
+            if (err)
+                res.send(err)
+            res.json({ message: 'Bear created!' });
+            // Bear.find(function (err, bears) {
+            //     if (err)
+            //         res.send(err);
+            //     console.log(bears);
+            //     res.json(bears);
+            // })
+            //res.json({ message: 'Bear created!' });
+        })
+    })
+    .get(function (req, res) {
+        console.log("request");
+        Bear.find(function (err, bears) {
+            if (err)
+                res.send(err);
+            console.log(bears);
+            res.json(bears);
+        });
+    });
+
+router.route('/bears/:bear_id')
+    .get(function (req, res) {
+        Bear.findById(req.params.bear_id, function (err, bear) {
+            if (err)
+                res.send(err);
+            console.log('bear', bear)
+            res.json(bear);
+        })
+    })
+    .put(function (req, res) {
+
+        // use our bear model to find the bear we want
+        Bear.findById(req.params.bear_id, function (err, bear) {
+
+            if (err)
+                res.send(err);
+
+            bear.text = req.body.text;  // update the bears info
+
+            // save the bear
+            bear.save(function (err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Bear updated!' });
+            });
+
+        });
+    })
+
+    .delete(function (req, res) {
+        Bear.remove({
+            _id: req.params.bear_id
+        }, function (err, bear) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
+
+//  app.get('/user', function(req,res){
+//       console.log('get is ',req.body);
+
+//    res.json({message : 'horaiiiiiiiiiiii  bhai cha gia '} );
+//  });
+
+// app.delete('/delete', function(req, res, next) {
+//   console.log("delete: req.body: " +  req.body.fareed);
+//   res.json({"data": req.body.fareed});
+// })
+
+
+app.use('/api', router);
+
+app.listen(port, function (req, res) {
+    console.log('the magic is here on ' + port)
+})
+
+
+
+
+
+  //some other code
+
+//app.use(passport.session());
+//app.use(flash());
 
 // router.get('/', function(req,res){
 //     res.json({message : 'horaiiiiiiiiiiii agaui bhai cha gia '} );
@@ -19,24 +135,6 @@ var router  = express.Router();
 // app.listen(port, function(req,res){
 //     console.log('the magic is here on '+ port)
 // })          // get an instance of the express Router
-
-
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.'); 
-    next(); // make sure we go to the next routes and don't stop here
-});
-
-
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
-
-
-
-
-
-
 // router.route('/bears')
 
 //     // create a bear (accessed at POST http://localhost:8080/api/bears)
@@ -58,17 +156,28 @@ router.get('/', function(req, res) {
 // // REGISTER OUR ROUTES -------------------------------
 // // all of our routes will be prefixed with /api
 // app.use('/api', router);
-router.route('/users')
-.post(function (req, res) {
-    console.log(req.body)
-     res.json({ message: req.body})
-     
-});
+// router.route('/users')
+// .post(function (req, res) {
+//     console.log(req.body)
+//      res.json({ message: req.body})
 
+// });
+// app.put('/update',function(req,res){
+//     post.findById(req.param.)
+// })
+//  app.put('/update',function(req, res) {
+//             post.findById(req.params.post_id, function(err, post) {
+//                 if (err)
+//                     res.send(err);
 
+//                 post.postTitle = req.body.postTitle;
 
-app.use('/api', router);
+//                 // save the post
+//                 post.save(function(err) {
+//                     if (err)
+//                         res.send(err);
 
-app.listen(port, function(req,res){
-    console.log('the magic is here on '+ port)
- })       
+//                     res.json({ message: 'post updated!' });
+//                 });
+//             });
+//         })
--- bear.js
+++ bear.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 var Schema  = mongoose.Schema;
  var BearSchema = new Schema({
-     name : String
+     name : String,
+    text:String,
+    data: String
  });
 
  module.exports = mongoose.model("Bear",BearSchema );
